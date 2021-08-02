fe2d0e5b55a67f9d26817ac9e92accc54baefe43
mark-obeirne <markobeirne00@gmail.com>
2021-08-02 17:38:05 +0100

    Complete exercise

---
+++ JavaScript/5kyu/paginationHelper.js
@@ -0,0 +1,73 @@
+/*
+////////////// PaginationHelper (5kyu) //////////////
+
+For this exercise you will be strengthening your page-fu mastery. You will complete the PaginationHelper class, which is a utility class helpful for querying paging information related to an array.
+
+The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. The types of values contained within the collection/array are not relevant.
+
+The following are some examples of how this class is used:
+
+var helper = new PaginationHelper(['a','b','c','d','e','f'], 4);
+helper.pageCount(); //should == 2
+helper.itemCount(); //should == 6
+helper.pageItemCount(0); //should == 4
+helper.pageItemCount(1); // last page - should == 2
+helper.pageItemCount(2); // should == -1 since the page is invalid
+
+// pageIndex takes an item index and returns the page that it belongs on
+helper.pageIndex(5); //should == 1 (zero based index)
+helper.pageIndex(2); //should == 0
+helper.pageIndex(20); //should == -1
+helper.pageIndex(-10); //should == -1
+
+*/
+
+// TODO: complete this object/class
+
+// The constructor takes in an array of items and a integer indicating how many
+// items fit within a single page
+function PaginationHelper(collection, itemsPerPage) {
+  this.collection = collection;
+  this.itemsPerPage = itemsPerPage;
+}
+
+// returns the number of items within the entire collection
+PaginationHelper.prototype.itemCount = function () {
+  return this.collection.length;
+};
+
+// returns the number of pages
+PaginationHelper.prototype.pageCount = function () {
+  return Math.ceil(this.itemCount() / this.itemsPerPage);
+};
+
+// returns the number of items on the current page. page_index is zero based.
+// this method should return -1 for pageIndex values that are out of range
+PaginationHelper.prototype.pageItemCount = function (pageIndex) {
+  if (pageIndex < 0 || pageIndex + 1 > this.pageCount()) return -1;
+
+  return (
+    this.itemsPerPage -
+    Math.ceil(
+      (((pageIndex + 1) * this.itemsPerPage) % this.itemCount()) %
+        this.itemsPerPage
+    )
+  );
+};
+
+// determines what page an item is on. Zero based indexes
+// this method should return -1 for itemIndex values that are out of range
+PaginationHelper.prototype.pageIndex = function (itemIndex) {
+  if (itemIndex < 0 || itemIndex + 1 > this.collection.length) return -1;
+  return Math.floor(itemIndex / this.itemsPerPage);
+};
+
+var helper = new PaginationHelper(["a", "b", "c", "d", "e", "f"], 4);
+console.log(helper.pageCount()); //should == 2
+console.log(helper.itemCount()); //should == 6
+console.log(helper.pageItemCount(0)); //should == 4
+console.log(helper.pageItemCount(1)); // last page - should == 2
+console.log(helper.pageItemCount(2)); // should == -1 since the page is invalid
+
+// .pageItemCount was a bit problematic and could probably be written cleaner…
+// Taken from https://github.com/a1essar/codewars/blob/master/5%20kyu.%20PaginationHelper.md
