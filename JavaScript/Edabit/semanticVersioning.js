9f2f5517291f2005298bdb8cdaf22fa28a62470a
mark-obeirne <markobeirne00@gmail.com>
2021-08-05 10:29:02 +0100

    Add completed semantic versioning challenge

---
+++ JavaScript/Edabit/semanticVersioning.js
@@ -0,0 +1,43 @@
+/*
+////////////// Semantic Versioning //////////////
+In semantic versioning a piece of software can be represented in a format like this example: 6.1.9.
+
+The first number is the major version.
+The second number is the minor version.
+The third number is the patch (bug fixes).
+Write three separate functions, one to retrieve each element in the semantic versioning specification.
+
+Examples
+// 6.1.9
+retrieveMajor("6.1.9") ➞ "6"
+
+retrieveMinor("6.1.9") ➞ "1"
+
+retrievePatch("6.1.9") ➞ "9"
+
+// 2.1.0
+retrieveMajor("2.1.0") ➞ "2"
+
+retrieveMinor("2.1.0") ➞ "1"
+
+retrievePatch("2.1.0") ➞ "0"
+
+*/
+
+function retrieveMajor(semver) {
+  return semver.split(".")[0];
+}
+
+function retrieveMinor(semver) {
+  return semver.split(".")[1];
+}
+
+function retrievePatch(semver) {
+  return semver.split(".")[2];
+}
+
+console.log(retrieveMajor("6.1.9")); // ➞ "6"
+
+console.log(retrieveMinor("6.1.9")); // ➞ "1"
+
+console.log(retrievePatch("6.1.9")); // ➞ "9"
